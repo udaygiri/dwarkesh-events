9c0619e89695d811d1483deab3be4a054705c841
Udaygiri D. Aparnathi <www.udayaparnathi2004@gmail.com>
2025-06-20 14:03:28 +0530

    setting update.

--- dwarkeshevents/settings.py
+++ dwarkeshevents/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['dwarkesh-events.onrender.com', 'localhost', '127.0.0.1']
 
